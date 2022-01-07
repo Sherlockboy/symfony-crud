7785544800037391c43a44e0cf41ba0b81d0194d
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2022-01-08 00:56:04 +0500

    fetch data

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,20 +2,59 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+    private $repository;
+    private $entityManager;
+    
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->entityManager = $doctrine->getManager();
+        $this->repository = $doctrine->getRepository(Article::class);
+    }
+    
     /**
      * @Route("/articles")
      * @Method({"GET"})
      */
     public function index()
     {
-        $articles = ['Article 1', 'Article 2', 'Article 3'];
+        $articles = $this->repository->findAll();
 
         return $this->render('articles/index.html.twig', compact('articles'));
+    }
+
+    /**
+     * @Route("/articles/{id}")
+     */
+    public function show(int $id)
+    {
+        $article = $this->repository->find($id);
+
+        return $this->render('articles/show.html.twig', compact('article'));
+    }
+
+    /**
+     * @Route("/articles/save")
+     * @Method({"GET"})
+     */
+    public function save() : Response
+    {   
+        $article = new Article();
+        $article->setTitle('Article 4');
+        $article->setBody('Body for Article 4');
+
+        $this->entityManager->persist($article);
+
+        $this->entityManager->flush();
+
+        return new Response("Saved article with id=" . $article->getId());
     }
 }
